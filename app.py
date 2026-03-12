6f193b48a35036fbd08d8d55c398b07307c340a7
Samuel Wehrli <samuel.wehrli@gmail.com>
2026-03-12 08:07:48 +0100

    rename one view file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if 'data_df' not in st.session_state:
 st.set_page_config(page_title="BMI Rechner", page_icon=":material/monitor_weight:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_second = st.Page("views/bmi_calculator.py", title="BMI-Rechner", icon=":material/calculate:")
+pg_second = st.Page("views/calculator.py", title="BMI-Rechner", icon=":material/calculate:")
 
 pg = st.navigation([pg_home, pg_second])
 pg.run()
